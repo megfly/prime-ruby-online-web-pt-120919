9335d94f11dd22e694b5692b06dae4f84ae15c46
Meghan Flynn <mflynn333@gmail.com>
2019-12-19 02:16:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
-def prime?(number)
-  return false if !number.integer?
-  return false if number < 2 
-  return true if number ==atan2(2..number-1).each {|int| return false if number % int == 0)
-  true 
+def prime?(x) 
+  count = 0 
+  for each in (2..x)
+  x% each == 0 ? count +=1 :
+  nil 
+end 
+return count == 1 ? true :
+false
 end 
